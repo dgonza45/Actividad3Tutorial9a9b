58423fec95be14f33a7c8f607bf38a10ef68d97e
dgonza45 <69641287+dgonza45@users.noreply.github.com>
2023-08-26 22:22:02 -0500

    Add files via upload

---
+++ Actividad3 - Tutorial 9a y 9b/pages/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreadoView, CartRemoveAllView, CartView, ImageViewFactory, ImageViewNoDI
+from .utils import ImageLocalStorage
+urlpatterns = [
+     path("", HomePageView.as_view(), name='home'),
+     path('about/', AboutPageView.as_view(), name='about'),
+     path('contact/', ContactPageView.as_view(), name='contact'),
+     path('products/', ProductIndexView.as_view(), name='index'),
+     path('products/create', ProductCreateView.as_view(), name='create'),
+     path('products/creado', ProductCreadoView.as_view(), name='creado'),
+     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+     path('cart/', CartView.as_view(), name='cart_index'),
+     path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+     path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+     path('image_save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+     path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenotdi_index'),
+     path('image/save', ImageViewNoDI.as_view(), name='imagenotdi_save'),
+
+
+
+
+     ]
